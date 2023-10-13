09529e82a6d0862452a0588b9e61b0d2765fd0ef
Lucas-Martin2023 <lucasmartin11990@gmail.com>
2023-10-12 21:39:48 -0500

    finished

--- components/forms/AuthorForm.js
+++ components/forms/AuthorForm.js
@@ -2,27 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import { Button, Form } from 'react-bootstrap';
-import { createBook, updateBook } from '../../api/bookData';
+import { createAuthor, updateAuthor } from '../../api/authorData';
 import { useAuth } from '../../utils/context/authContext';
-import { getAuthors } from '../../api/authorData';
 
 const intialState = {
-  title: '',
-  description: '',
   image: '',
-  price: '',
-  sale: false,
+  first_name: '',
+  last_name: '',
+  favorite: '',
+  email: '',
 };
 
-function BookForm({ obj }) {
+function AuthorForm({ obj }) {
   const [formInput, setFormInput] = useState(intialState);
-  const [authors, setAuthors] = useState([]);
   const router = useRouter();
   const { user } = useAuth();
 
   useEffect(() => {
     if (obj.firebaseKey) setFormInput(obj);
-    getAuthors(user.uid).then(setAuthors);
   }, [obj, user]);
 
   const handleChange = (e) => {
@@ -37,14 +34,14 @@ function BookForm({ obj }) {
     e.preventDefault();
 
     if (obj.firebaseKey) {
-      updateBook(formInput).then(() => router.push(`/book/${obj.firebaseKey}`));
+      updateAuthor(formInput).then(() => router.push(`/author/${obj.firebaseKey}`));
     } else {
       const payload = { ...formInput, uid: user.uid };
-      createBook(payload).then(({ name }) => {
+      createAuthor(payload).then(({ name }) => {
         const patchPayload = { firebaseKey: name };
 
-        updateBook(patchPayload).then(() => {
-          router.push('/books');
+        updateAuthor(patchPayload).then(() => {
+          router.push('/authors');
         });
       });
     }
@@ -54,32 +51,33 @@ function BookForm({ obj }) {
     <>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Book Title</Form.Label>
+          <Form.Label>Author First Name</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Enter Book Title"
-            name="title"
-            value={formInput.title}
+            placeholder="Enter Author's First Name"
+            name="first_name"
+            value={formInput.first_name}
             onChange={handleChange}
             required
           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Description</Form.Label>
+          <Form.Label>Author Last Name</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Enter Book Description"
-            name="description"
-            value={formInput.description}
+            placeholder="Enter Author's Last Name"
+            name="last_name"
+            value={formInput.last_name}
             onChange={handleChange}
+            required
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Image</Form.Label>
           <Form.Control
             type="url"
-            placeholder="Enter Book Image"
+            placeholder="Enter Author's Image"
             name="image"
             value={formInput.image}
             onChange={handleChange}
@@ -87,17 +85,17 @@ function BookForm({ obj }) {
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Price</Form.Label>
+          <Form.Label>Author Email</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Enter Book Price"
-            name="price"
-            value={formInput.price}
+            placeholder="Enter Author's Email"
+            name="email"
+            value={formInput.email}
             onChange={handleChange}
             required
           />
         </Form.Group>
-        <Form.Select
+        {/* <Form.Select
           name="author_id"
           onChange={handleChange}
           value={obj.author_id}
@@ -116,43 +114,42 @@ function BookForm({ obj }) {
           ))
         }
 
-        </Form.Select>
+        </Form.Select> */}
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check
             type="switch"
-            label="On Sale?"
-            name="sale"
-            checked={formInput.sale}
+            label="Favorite?"
+            name="favorite"
+            checked={formInput.favorite}
             onChange={(e) => {
               setFormInput((prevState) => ({
                 ...prevState,
-                sale: e.target.checked,
+                favorite: e.target.checked,
               }));
             }}
           />
         </Form.Group>
         <Button variant="primary" type="submit">
-          {obj.firebaseKey ? 'Update Book' : 'Submit Book'}
+          {obj.firebaseKey ? 'Update Author' : 'Submit Author'}
         </Button>
       </Form>
     </>
   );
 }
 
-BookForm.propTypes = {
+AuthorForm.propTypes = {
   obj: PropTypes.shape({
-    description: PropTypes.string,
     image: PropTypes.string,
-    price: PropTypes.string,
-    sale: PropTypes.bool,
-    title: PropTypes.string,
-    author_id: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    favorite: PropTypes.bool,
+    email: PropTypes.string,
     firebaseKey: PropTypes.string,
   }),
 };
 
-BookForm.defaultProps = {
+AuthorForm.defaultProps = {
   obj: intialState,
 };
 
-export default BookForm;
+export default AuthorForm;
